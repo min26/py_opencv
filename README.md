376692bb89ca24aefe35f3ab5825f823d5e1c63e
min <min@email.com>
2024-10-06 14:41:51 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OpenCV with Anaconda package
+
+## conda install
+> $> conda install conda-forge::openvc
+> $> conda install conda-forge::matplotlib
+
+
--- myopencv.py
+++ myopencv.py
@@ -9,43 +9,45 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# # 1. read/write 
-#
-# # cv2.imread(path, flag)
-# # - flag:   cv2.IMREAD_COLOR (default)
-# #           cv2.IMREAD_GRAYSCALE
-# #           cv2.IMREAD_UNCHANGED
-# image = cv2.imread("dog.jpg", cv2.IMREAD_GRAYSCALE)
-# # 
-# cv2.imwrite('graydog.png', image)
-
-# # 2. show image and key wait
-#
-# image = cv2.imread("dog.jpg")
-# print (image.shape)
-# # cv2.imshow('title', image)
-# cv2.imshow("Dog", image)
-# # cv2.waitkey(milliseconds) 0 = until close
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# # 2.1. image path
+# --- 1. read image --------------------
+## cv2.imread(path, <flag>)
+## - 1.1. flag
+## cv2.IMREAD_COLOR (default)
+## cv2.IMREAD_GRAYSCALE
+## cv2.IMREAD_UNCHANGED
+## - 1.1. path
 # impath = '/home/min/Document/py_opendv/dog.jpg'
-# image = cv2.imread(path, 0) ## 0 = cv2.IMREAD_GRAYSCALE
-# cv2.imshow("Dog", image)
-
-
-# 3. convert colors
+# grayimage = cv2.imread(impath, 0) ## 0 = cv2.IMREAD_GRAYSCALE
+# grayimage = cv2.imread("dog.jpg", cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("dog.jpg") ## cv2.IMAGE_COLOR
+print (image.shape)
+#
+# --- 2. convert image --------------------
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#
+# --- 3. write image -----------
+## support format: jpeg, png, tiff, bmp, ppm, pgm
+new_dir = '/home/min/Documents'
+os.chdir(new_dir)
+success = cv2.imwrite('graydog.png', gray_image)
+if not success:
+    print("ERROR write image")
+else:
+    print(os.listdir(new_dir))
 # 
-image = cv2.imread("dog.jpg")
-# # Convert BGR to RGB
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# #
-plt.imshow(rgb_image)
-plt.waitforbuttonpress()
-plt.close('all')
 
-
+#
+# --- 4. show image ----------------------
+# - 4.1. cv2.imshow('title', image)
+cv2.imshow("Dog", gray_image)
+cv2.waitKey(0) ## milliseconds
+cv2.destroyAllWindows()
+#
+# - 4.2. matplotlib.imshow(image)
+# plt.imshow(gray_image)
+# plt.waitforbuttonpress()
+# plt.close('all')
 
 
 # img = cv2.imread("dog.jpg")
