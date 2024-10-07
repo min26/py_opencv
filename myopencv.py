1e33d397a31022104b03e04fb00acf4ede42665c
min <min@email.com>
2024-10-07 16:08:56 -0700

    image rotate

--- myopencv.py
+++ myopencv.py
@@ -23,36 +23,56 @@ class CV2test:
         # cv2.IMREAD_UNCHANGED
         # - 1.1. path
         impath = '/home/min/Document/py_opendv/dog.jpg'
-        grayimage = cv2.imread(impath, 0) ## 0 = cv2.IMREAD_GRAYSCALE
-        grayimage = cv2.imread("dog.jpg", cv2.IMREAD_GRAYSCALE)
         """
         image = cv2.imread("images/dog.jpg") ## cv2.IMAGE_COLOR
         print (image.shape)
         #
         # --- 2. convert image --------------------
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # -- gray image#2
+        # cvt_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # -- rgb image
+        cvt_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         #
         # --- 3. write image -----------
         ## support format: jpeg, png, tiff, bmp, ppm, pgm
         new_dir = '/home/min/Documents'
         os.chdir(new_dir)
-        success = cv2.imwrite('graydog.png', gray_image)
+        success = cv2.imwrite('graydog.png', cvt_image)
         if not success:
             print("ERROR write image")
         else:
             print(os.listdir(new_dir))
         # 
         # --- 4. show image ----------------------
-        # - 4.1. cv2.imshow('title', image)
-        cv2.imshow("Dog", gray_image)
+        # -- 4.1. cv2.imshow('title', image)
+        cv2.imshow("Convert", cvt_image)
         cv2.waitKey(0) ## milliseconds
         cv2.destroyAllWindows()
         #
-        # - 4.2. matplotlib.imshow(image)
+        # -- 4.2. matplotlib.imshow(image)
         # plt.imshow(gray_image)
         # plt.waitforbuttonpress()
         # plt.close('all')
+
+    def gray_image(self):
+        ## - 1. read as gray
+        # grayimage = cv2.imread(impath, 0) ## 0 = cv2.IMREAD_GRAYSCALE
+        # grayimage = cv2.imread("dog.jpg", cv2.IMREAD_GRAYSCALE)
+        #        
+        image = cv2.imread("image/dog.jpg")
+        ## - 2. convert as gray
+        # grayimage = cv2.cvtColor(image, cv2.COLOR_COLOR_BGR2GRAY)
+        #
+        ## - 3. pixel manipulation
+        row, col = image.shape[0:2]
+        ## BGR channel to convert 'colored' to 'gray'
+        for i in range(row):
+            for j in range(col):
+                grayimage[i, j] = sum(image[i,j]) * 0.33
+        ###
+        cv2.imshow('Grayscale',grayimage)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def arithmetics(self):
         """
@@ -105,26 +125,104 @@ class CV2test:
         ---- cv2.INTER_NEAREST: nearest neighbor
         ---- cv2.INTER_LINEAR: primarily zooming. default. 
         """
-        image = cv2.imread("images/dog.jpg")
-        ## get image dimensions
-        h,w = image.shape[:2]
-        aspect_ratio = h/w
-        ## desired width
-        new_width = 800         
-        new_height = int(new_width * aspect_ratio)
+        image = cv2.imread("images/elephant.png")
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        h,w = image_rgb.shape[:2]
+        ## 1. Change dimensions with same ratio        
+        #aspect_ratio = h/w
+        #new_width = 800         
+        #new_height = int(new_width * aspect_ratio)
+        #
+        ## 2. Change with scale factor
+        scale1 = 3.0    #increase 3x
+        scale2 = 1/3.0  #decrease 3x
+        new_width = int(w * scale1)
+        new_height = int(h * scale1)
         ##
-        ratio = cv2.resize(image, (new_width, new_height))
-        half = cv2.resize(image, (0,0), fx=0.1, fy=0.1)        
-        stretch = cv2.resize(image, (780,540),interpolation=cv2.INTER_LINEAR)
-        titles=["origin","ratio","half","stretch"]
-        images=[image, ratio, half, stretch]
+        scale = cv2.resize(image_rgb, 
+                            dsize=(new_width, new_height),
+                            interpolation=cv2.INTER_AREA)
+        zoom = cv2.resize(image_rgb, 
+                            dsize=(new_width, new_height),
+                            interpolation=cv2.INTER_CUBIC)
+        half = cv2.resize(image_rgb, (0,0), fx=0.1, fy=0.1)        
+        #stretch = cv2.resize(image_rgb, (780,540),interpolation=cv2.INTER_LINEAR)
+        #
+        titles=["origin","scale","zoom","half"]
+        images=[image_rgb, scale, zoom, half]
         ##
         for i in range(4):
             plt.subplot(2,2, i+1)
             plt.title(titles[i])
             plt.imshow(images[i])
+        plt.tight_layout()
         plt.show()
 
+    def imgRotation(self):
+        """
+        cv2.getRotationMatrix2D()
+        """
+
+    def imgErode(self):
+        """
+        [erode] reduce,...
+        cv2.erode(src, kernel, [dst], [anchor], [interations], 
+                    [borderType], [boarderValue])
+        -- src: input image
+        -- kernel: structuring element used for erosion
+        -- dst: output image
+        -- anchor: default point is kernel center(-1, -1)
+        -- iterations: number of times erosion is applied
+        -- borderType: cv2.BORDER_CONSTANT, cv2.BORDER_REFLECT,...
+        -- borderValue: returns an images
+        - returns an output image
+        """
+        path = "images/dog.jpg"
+        image = cv2.imread(path)
+        ## 1. erodsion
+        kernel = np.ones((5,5), np.uint8)
+        new_image = cv2.erode(image, kernel)
+        # new_image = cv2.erode(image, kernel, cv2.BORDER_REFLECT)
+        #
+        ## 2. Make border 
+        ## copyMakeBorder(src, top,bottom,left,right, borderType, value)
+        ## -- borderType: cv2.BOARDER_DEFAULT, _CONSTANT, _REFLECT
+        ## -- value (optional): cv2.BORDER_CONSTANT
+        # new_image = cv2.copyMakeBorder(new_image, 5,5,5,5, cv2.BORDER_CONSTANT)
+        new_image = cv2.copyMakeBorder(new_image, 100,100,100,100, cv2.BORDER_REFLECT)
+        #
+        cv2.imshow("Erosion", new_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def imgBlur(self):
+        """
+        [blurring]
+        -- remove noise, smoothing image, hiding details
+        + filter2D(src, ddepth, kernel)
+        + blur(src, shapeOfKernel)
+        + GaussianBlur(src,shapeOfKernel, sigmaX): reduce image noise and detail
+        + medianBlur(src, kernelSize): non-linear digital filter, best to remove salt and pepper noise.
+        + bilateralFilter(src, diameter, sigmaColor, sigmaSpacce): non-linear, edge-preserving, noise-reducing. 
+        Replaces each pixel with average intensity values from nearby pixels.
+        --- shpeOfKernel: matrix 3x3, 5x5, ...
+        --- kernelSize: positive integer more than 2.
+        --- diameter: similar to kernelSize
+        --- sigmaColor: number of color range of pixels
+        --- sigmaSpace: space between pixels
+        """
+        path = "images/dog.jpg"
+        image = cv2.imread(path)
+        kernel_val = np.ones((5,5), np.float32)/25
+        ## blurring
+        # blurImg = cv2.filter2D(src=image, ddepth=-1, kernel_val)
+        # blurImg = cv.blur(image, (5,5))
+        # blurImg = cv2.Gaussianblur(image, (7,7), 0)
+        blurImg = cv2.medianBlur(image, 9)
+        # blurImg = cv2.bilateralFilter(image, 9, 75, 75)
+        cv2.imshow('BlurImg', blurImg)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     mycv = CV2test()
